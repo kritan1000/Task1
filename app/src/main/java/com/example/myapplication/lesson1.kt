b0f19f298ac1460b95e915a293c3e68e21a0809b
kritan nepal <kritannepal11@gmail.com>
2025-04-21 09:39:14 +0545

    lesson1

---
+++ app/src/main/java/com/example/myapplication/lesson1.kt
@@ -0,0 +1,34 @@
+package com.example.firstclass
+fun main(){
+//types of variable
+// mutablevar
+// age=19;
+// age=10;
+// immutable
+// val roll=10;//
+// roll=11;
+// cannot reassign value
+// var a: Boolean=true    
+// var b:Char='R'
+// var c:Byte=12
+// var d: Short=-356
+// var e:Int=43543
+// var f:Long=-51321354L
+// var i: Float=5.6451344F
+// var h: Double=7.32644564
+// println(a)
+// println(b)
+// println(c)
+// println(d)
+// println(e)
+// println(f)
+// println(i)
+// println(h)
+// Type conversion
+// var x: Double=132.321;
+// var y:Int=x.toInt()
+// var z: Byte=y.toByte()
+// println(x)
+// println(y)
+// println(z)}
+}
